3759cc082bb4974bd6fe6118e175562701dc3c05
ledgerc <30798256+ledgerc@users.noreply.github.com>
2021-02-02 11:06:53 +0800

    Update LCA.dfs+ST.cpp

--- ACM/模版/图论/LCA.dfs+ST.cpp
+++ ACM/模版/图论/LCA.dfs+ST.cpp
@@ -2,116 +2,116 @@
 HDU 5296
 */
 struct Edge{
-	int u,v,w,next;
+  int u,v,w,next;
 };
 struct LCA{
-	int dp[2*maxn][50];
-	int tot,head[maxn];
-	Edge edge[maxn*2];
-	void addedge(int u,int v,int w,int &cnt){
-		edge[cnt].u=u; edge[cnt].v=v; edge[cnt].w=w;
-		edge[cnt].next=head[u]; head[u]=cnt++;
-	}
-	//Éî¶È ¾àÀë 
-	int d[maxn],dis[maxn];
-	//Å·À­ĞòÁĞ£¬¾ÍÊÇdfs±éÀúµÄË³Ğò£¬³¤¶ÈÎª2*n-1,ÏÂ±ê´Ó1¿ªÊ¼
-	//±íÊ¾µãiÔÚverÖĞµÚÒ»´Î³öÏÖµÄÎ»ÖÃ(½ÚµãiµÄdfsĞò)
-	int ver[2*maxn],first[maxn];
-	bool vis[maxn];
-	void init(){
-		tot=0; dis[1]=0;
-		memset(head,-1,sizeof head);
-		memset(vis,false,sizeof vis);
-	}
-	void dfs(int u,int dep,int pre){
-		vis[u]=true;
-		ver[++tot]=u;
-		first[u]=tot;
-		d[tot]=dep;
-		for(int i=head[u]; i!=-1; i=edge[i].next){
-			int v = edge[i].v,w = edge[i].w;
-			if(vis[v])
-				continue;
-			dis[v]=dis[u]+w;
-			dfs(v,dep+1,u);
-			ver[++tot]=u;
-			d[tot]=dep;
-		}
-	}
-	void ST(int n){
-		for(int i=1; i<=n; i++)
-			dp[i][0]=i;
-		for(int j=1; (1<<j)<=n; j++)
-			for(int i=1; i+(1<<j)-1<=n; i++){
-				int a=dp[i][j - 1],b=dp[i+(1<<(j-1))][j-1];
-				dp[i][j]=d[a]<d[b]?a:b;
-			}
-	}
-	int RMQ(int l,int r){
-		int k=0;
-		while((1<<(k+1))<=r-l+1)
-			k++;
+  int dp[2*maxn][50];
+  int tot,head[maxn];
+  Edge edge[maxn*2];
+  void addedge(int u,int v,int w,int &cnt){
+    edge[cnt].u=u; edge[cnt].v=v; edge[cnt].w=w;
+    edge[cnt].next=head[u]; head[u]=cnt++;
+  }
+  //æ·±åº¦ è·ç¦» 
+  int d[maxn],dis[maxn];
+  //æ¬§æ‹‰åºåˆ—ï¼Œå°±æ˜¯dfséå†çš„é¡ºåºï¼Œé•¿åº¦ä¸º2*n-1,ä¸‹æ ‡ä»1å¼€å§‹
+  //è¡¨ç¤ºç‚¹iåœ¨verä¸­ç¬¬ä¸€æ¬¡å‡ºç°çš„ä½ç½®(èŠ‚ç‚¹içš„dfsåº)
+  int ver[2*maxn],first[maxn];
+  bool vis[maxn];
+  void init(){
+    tot=0; dis[1]=0;
+    memset(head,-1,sizeof head);
+    memset(vis,false,sizeof vis);
+  }
+  void dfs(int u,int dep,int pre){
+    vis[u]=true;
+    ver[++tot]=u;
+    first[u]=tot;
+    d[tot]=dep;
+    for(int i=head[u]; i!=-1; i=edge[i].next){
+      int v = edge[i].v,w = edge[i].w;
+      if(vis[v])
+        continue;
+      dis[v]=dis[u]+w;
+      dfs(v,dep+1,u);
+      ver[++tot]=u;
+      d[tot]=dep;
+    }
+  }
+  void ST(int n){
+    for(int i=1; i<=n; i++)
+      dp[i][0]=i;
+    for(int j=1; (1<<j)<=n; j++)
+      for(int i=1; i+(1<<j)-1<=n; i++){
+        int a=dp[i][j - 1],b=dp[i+(1<<(j-1))][j-1];
+        dp[i][j]=d[a]<d[b]?a:b;
+      }
+  }
+  int RMQ(int l,int r){
+    int k=0;
+    while((1<<(k+1))<=r-l+1)
+      k++;
         int a=dp[l][k],b=dp[r-(1<<k)+1][k];
         return d[a]<d[b]?a:b;
-	}
-	int Lca(int u,int v){
-		int x = first[u], y = first[v];
-		if(x>y)
-			swap(x,y);
-		return ver[RMQ(x,y)];
-	}
+  }
+  int Lca(int u,int v){
+    int x = first[u], y = first[v];
+    if(x>y)
+      swap(x,y);
+    return ver[RMQ(x,y)];
+  }
 }lca;
 set<int> s; set<int>::iterator it;
-bool vis[maxn];//ÅĞ¶ÏµãÊÇ·ñÔÚ¼¯ºÏÖĞ
+bool vis[maxn];//åˆ¤æ–­ç‚¹æ˜¯å¦åœ¨é›†åˆä¸­
 int calc(int u){
-	if(s.empty())
-		return 0;
-	it=s.upper_bound(u);
-	int x,y;//x,y·Ö±ğÊÇ±ÈdfsĞòÎªuµÄµã´óµÄ×îĞ¡µãºÍ±ÈËüĞ¡µÄ×î´óµã
-	if(it==s.end()||it==s.begin()){
-		x = lca.ver[*s.rbegin()];
-		y = lca.ver[*s.begin()];
-	}
-	else{
-		x = lca.ver[*it];
-		it--;
-		y = lca.ver[*it];
-	}
-	u = lca.ver[u];
-	return lca.dis[u] - lca.dis[lca.Lca(x,u)] - lca.dis[lca.Lca(y,u)] + lca.dis[lca.Lca(x,y)];
+  if(s.empty())
+    return 0;
+  it=s.upper_bound(u);
+  int x,y;//x,yåˆ†åˆ«æ˜¯æ¯”dfsåºä¸ºuçš„ç‚¹å¤§çš„æœ€å°ç‚¹å’Œæ¯”å®ƒå°çš„æœ€å¤§ç‚¹
+  if(it==s.end()||it==s.begin()){
+    x = lca.ver[*s.rbegin()];
+    y = lca.ver[*s.begin()];
+  }
+  else{
+    x = lca.ver[*it];
+    it--;
+    y = lca.ver[*it];
+  }
+  u = lca.ver[u];
+  return lca.dis[u] - lca.dis[lca.Lca(x,u)] - lca.dis[lca.Lca(y,u)] + lca.dis[lca.Lca(x,y)];
 }
 int main(){
-	int T; scanf("%d",&T);
-	for(int kase=1; kase<=T; kase++){
-		int n,m; scanf("%d%d",&n,&m);
-		memset(vis,false,sizeof vis);
-		s.clear(); lca.init(); int cnt=0;
-		for(int i=1; i<n; i++){
-			int u,v,w; scanf("%d%d%d",&u,&v,&w);
-			lca.addedge(u,v,w,cnt);
-			lca.addedge(v,u,w,cnt);
-		}
-		lca.dfs(1,-1,-1); lca.ST(2*n-1);
-		printf("Case #%d:\n",kase);
-		int op,u,ans=0;
-		while(m--){
-			scanf("%d%d",&op,&u);
-			if(op==1){
-				if(!vis[u]){
-					vis[u]=true;
-					ans += calc(lca.first[u]);
-					s.insert(lca.first[u]);
-				}
-			}
-			else{
-				if(vis[u]){
-					vis[u]=false;
-					s.erase(lca.first[u]);
-					ans-=calc(lca.first[u]);
-				}
-			}
-			printf("%d\n",ans);
-		}
-	}
-	return 0;
+  int T; scanf("%d",&T);
+  for(int kase=1; kase<=T; kase++){
+    int n,m; scanf("%d%d",&n,&m);
+    memset(vis,false,sizeof vis);
+    s.clear(); lca.init(); int cnt=0;
+    for(int i=1; i<n; i++){
+      int u,v,w; scanf("%d%d%d",&u,&v,&w);
+      lca.addedge(u,v,w,cnt);
+      lca.addedge(v,u,w,cnt);
+    }
+    lca.dfs(1,-1,-1); lca.ST(2*n-1);
+    printf("Case #%d:\n",kase);
+    int op,u,ans=0;
+    while(m--){
+      scanf("%d%d",&op,&u);
+      if(op==1){
+        if(!vis[u]){
+          vis[u]=true;
+          ans += calc(lca.first[u]);
+          s.insert(lca.first[u]);
+        }
+      }
+      else{
+        if(vis[u]){
+          vis[u]=false;
+          s.erase(lca.first[u]);
+          ans-=calc(lca.first[u]);
+        }
+      }
+      printf("%d\n",ans);
+    }
+  }
+  return 0;
 }
